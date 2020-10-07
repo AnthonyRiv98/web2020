ba9c0e603a1ab32adcfc47b1f53cf7122fc298ca
AnthonyRiv98 <55204439+AnthonyRiv98@users.noreply.github.com>
2020-10-07 14:32:15 -0400

    php

---
+++ action.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+    <head>
+    
+        <?php
+           $pcodeErr = $emailErr = $numberErr = "";
+           $email = $pcode = $number = "";
+            
+             if (empty($_POST["email"])) {
+               $emailErr = "Email is required";
+             } else {
+               $email = test_input($_POST["email"]);
+               // check if e-mail address is well-formed
+               if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                 $emailErr = "Invalid email format";
+               }
+             }
+               
+             if (empty($_POST["number"])) {
+               $number = "";
+             } else {
+               $number = test_input($_POST["website"]);
+               // check if URL address syntax is valid
+               if (!preg_match('/^[0-9]*$/',$website)) {
+                 $numberErr = "Invalid URL";
+               }    
+             }
+           
+             echo ($email);
+             echo($number);
+
+        ?>
+    </head>
+</html>
